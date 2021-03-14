ed18730282f55549060ac40978ebfab59a6fb53f
Wesley Davies <wtd224@nyu.edu>
2021-03-14 15:05:00 -0400

    fixed. terrible. collision. bug

--- Assets/Scripts/States/PlayerFSM/PlayerStateEmptyHanded.cs
+++ Assets/Scripts/States/PlayerFSM/PlayerStateEmptyHanded.cs
@@ -18,7 +18,7 @@ public class PlayerStateEmptyHanded : PlayerState
     public override void Update(Player player) {
         //OverlapCapsule calculations based on this: https://roundwide.com/physics-overlap-capsule/
         var center = player.transform.TransformPoint(playerCollider.center);
-        var size = player.transform.TransformVector(playerCollider.radius, playerCollider.height, playerCollider.radius);
+        var size = new Vector3(playerCollider.radius, playerCollider.height, playerCollider.radius); //the code given in the above link for this line is WRONG!
         var radius = size.x; //the CharacterController radius + skinWidth MUST be smaller than the CapsuleCollider radius or else it won't detect collisions!
         var height = size.y;
         var bottom = new Vector3(center.x, center.y - height / 2 + radius, center.z);
