5183ec5178fdbc070a6c66d521d2d06ba9c10d1e
Wesley Davies <wtd224@nyu.edu>
2021-03-13 20:39:39 -0500

    fixed collision bug

--- Assets/Scripts/States/PlayerFSM/PlayerStateEmptyHanded.cs
+++ Assets/Scripts/States/PlayerFSM/PlayerStateEmptyHanded.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerStateEmptyHanded : PlayerState
 {
     private CapsuleCollider capsuleCollider;
-    private CharacterController controller;
     private Collider[] overlapBall = new Collider[1];
     private bool oldCheckCapsule;
     private bool hasBall;
@@ -14,14 +13,13 @@ public class PlayerStateEmptyHanded : PlayerState
 
     public override void Enter(Player player) {
         capsuleCollider = player.GetComponent<CapsuleCollider>();
-        controller = player.GetComponent<CharacterController>();
         ballMask = LayerMask.GetMask("Ball");
     }
     public override void Update(Player player) {
         //OverlapCapsule calculations based on this: https://roundwide.com/physics-overlap-capsule/
         var center = player.transform.TransformPoint(capsuleCollider.center);
         var size = player.transform.TransformVector(capsuleCollider.radius, capsuleCollider.height, capsuleCollider.radius);
-        var radius = size.x + controller.skinWidth;
+        var radius = size.x; //the CharacterController radius MUST be smaller than the CapsuleCollider radius or else it won't detect collisions!
         var height = size.y;
         var bottom = new Vector3(center.x, center.y - height / 2 + radius, center.z);
         var top = new Vector3(center.x, center.y + height / 2 - radius, center.z);
@@ -33,7 +31,6 @@ public class PlayerStateEmptyHanded : PlayerState
             hasBall = true;
             if (overlapBall[0].gameObject.GetComponent<Blastball>() != null)
             {
-                Debug.Log("penis");
                 //player has the blastball
             }
         }
