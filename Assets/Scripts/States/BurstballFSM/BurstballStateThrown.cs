ed8b35dbb6cab05ed6cb7d2d5abd1b63d45ec18b
Wesley Davies <wtd224@nyu.edu>
2021-03-15 00:19:32 -0400

    fixed burstball collision, created burstball texture

--- Assets/Scripts/States/BurstballFSM/BurstballStateThrown.cs
+++ Assets/Scripts/States/BurstballFSM/BurstballStateThrown.cs
@@ -7,14 +7,16 @@ public class BurstballStateThrown : BurstballState
     private float blastRadius;
     private float blastMagnitude;
     private float blastLift; //how much the blast should lift objects
+    private LayerMask courtMask;
 
     public override void Enter(Burstball burstball) {
         blastRadius = 10f;
         blastMagnitude = 10f;
         blastLift = 0f;
+        courtMask = LayerMask.GetMask("Court");
     }
     public override void Update(Burstball burstball) {
-        if (Physics.CheckSphere(burstball.transform.position, burstball.transform.lossyScale.x / 2))
+        if (Physics.CheckSphere(burstball.transform.position, burstball.transform.lossyScale.x / 2, courtMask)) //TODO: ideally, burstballs should explode on contact with anything, but it was just immediately exploding. Perhaps it was colliding with itself?
         {
             burstball.GetComponent<Ball>().Blast(burstball.transform.position, blastRadius, blastMagnitude, blastLift);
         }
